3bab246ba304702ef517653a1f57e3c7a68f9808
Douglas Tinoco <douglas.samuel1234@gmail.com>
2024-02-28 03:03:33 -0300

    Adding data streaming

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Dashboard</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Dashboard" />
+      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
